4bd580f97255b0a5c8f74fafb6d78d8826157dfa
ArthurPrestes <66209226+ArthurPrestes@users.noreply.github.com>
2021-04-22 00:28:07 -0300

    netflix
    first push

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public stories: Array<any> = new Array<any>();
+  public movies: Array<any> = new Array<any>();
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() { }
+
+  ionViewidEnter() {
+    this.loadStories();
+    this.loadMovies();
+  }
+
+  loadStories() {
+  this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1').subscribe(data => {
+    const response: any = data;
+
+    this.movies = response.Search;
+});
+}
+
+loadMovies() {
+  this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&page=1').subscribe(data => {
+    const response: any = data;
+
+    this.movies = response.Serach;
+  });
+}
+}
